bb4ff2e36f12ae6e7a9d1ada7f80b03e42568300
LightQuantum <self@lightquantum.me>
2023-02-06 01:53:18 +0800

    fix: cli parser

--- rsync-fetcher/src/opts.rs
+++ rsync-fetcher/src/opts.rs
@@ -8,23 +8,31 @@ use crate::rsync::filter::Rule;
 #[derive(Parser)]
 pub struct Opts {
     /// Rsync remote url.
+    #[clap(long)]
     pub src: Url,
     /// S3 endpoint url.
     /// For specifying authentication, use environment variables:
     /// AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+    #[clap(long)]
     pub s3_url: Url,
     /// S3 storage region.
+    #[clap(long)]
     pub s3_region: String,
     /// S3 storage bucket.
+    #[clap(long)]
     pub s3_bucket: String,
     /// S3 storage prefix.
+    #[clap(long)]
     pub s3_prefix: String,
     /// Metadata storage url. (Redis)
+    #[clap(long)]
     pub redis: Url,
     /// Metadata namespace. Need to be unique for each repository.
+    #[clap(long)]
     pub redis_namespace: String,
     /// Force break existing lock.
     /// Only use this if you are sure there's no other fetch process running on the same namespace.
+    #[clap(long)]
     pub force_break: bool,
     /// Exclude files matching given pattern.
     #[clap(long, action = ArgAction::Append)]
@@ -45,11 +53,16 @@ pub struct S3Opts {
 
 impl From<&Opts> for S3Opts {
     fn from(opts: &Opts) -> Self {
+        let prefix = if opts.s3_prefix.ends_with('/') {
+            opts.s3_prefix.clone()
+        } else {
+            format!("{}/", opts.s3_prefix)
+        };
         Self {
             region: opts.s3_region.clone(),
             url: opts.s3_url.clone(),
             bucket: opts.s3_bucket.clone(),
-            prefix: opts.s3_prefix.clone(),
+            prefix,
         }
     }
 }
