c8028d97371f3d3c1ad0d142c367cd514a01d7b9
LightQuantum <self@lightquantum.me>
2023-02-10 02:40:30 +0800

    fix(gateway): non-ascii filenames are not handled correctly

--- rsync-gateway/src/handler.rs
+++ rsync-gateway/src/handler.rs
@@ -1,5 +1,6 @@
 use actix_web::web::{Data, Redirect};
 use actix_web::{Either, HttpRequest, HttpResponse, Responder};
+use bstr::{BStr, ByteSlice};
 use tracing::debug;
 
 use rsync_core::utils::ToHex;
@@ -10,23 +11,25 @@ use crate::utils::{ReportExt, ReportWrapper};
 
 /// Main handler.
 pub async fn handler(opts: Data<Opts>, state: Data<State>, req: HttpRequest) -> impl Responder {
-    let path = req.uri().path();
+    let path = urlencoding::decode_binary(req.path().as_bytes());
 
-    debug!(%path, "incoming request");
+    debug!(path=?BStr::new(&path), "incoming request");
 
-    let listing = path.ends_with('/');
+    let listing = path.ends_with(b"/");
+    let path = path.as_ref().trim_start_with(|c| c == '/');
     if listing {
-        Either::Left(listing_handler(&opts, &state, path.trim_start_matches('/')))
+        Either::Left(listing_handler(&opts, &state, path))
     } else {
-        Either::Right(file_handler(&opts, &state, path.trim_start_matches('/')).await)
+        Either::Right(file_handler(&opts, &state, path).await)
     }
 }
 
 /// Handler for listing requests.
-fn listing_handler(opts: &Opts, state: &State, path: &str) -> impl Responder {
+fn listing_handler(opts: &Opts, state: &State, path: &[u8]) -> impl Responder {
     state.latest_index().map_or_else(
         || Either::Right(HttpResponse::NotFound()),
         |latest| {
+            let path = urlencoding::encode_binary(path);
             let s3_base = opts.s3_base.trim_end_matches('/');
             Either::Left(Redirect::to(format!(
                 "{s3_base}/listing-{latest}/{path}index.html"
@@ -36,12 +39,9 @@ fn listing_handler(opts: &Opts, state: &State, path: &str) -> impl Responder {
 }
 
 /// Handler for file requests.
-async fn file_handler(opts: &Opts, state: &State, path: &str) -> impl Responder {
+async fn file_handler(opts: &Opts, state: &State, path: &[u8]) -> impl Responder {
     let s3_base = opts.s3_base.trim_end_matches('/');
-    let hash = state
-        .lookup_hash_of_path(path.trim_start_matches('/').as_bytes())
-        .await
-        .into_resp_err()?;
+    let hash = state.lookup_hash_of_path(path).await.into_resp_err()?;
     Ok::<_, ReportWrapper>(hash.map_or_else(
         || Either::Right(HttpResponse::NotFound()),
         |hash| Either::Left(Redirect::to(format!("{s3_base}/{:x}", hash.as_hex()))),
